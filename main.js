2371d2d20a3c4ff2f855e0e56dd198af89c07cf1
j3ffh95 <j3ffh95@gmail.com>
2020-10-01 10:13:12 -0400

    Added logic to add and remove class of active

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const open = document.querySelector("#open"),
   close = document.querySelector("#close"),
   container = document.querySelector(".popup-container");
+
+open.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  container.classList.remove("active");
+});
